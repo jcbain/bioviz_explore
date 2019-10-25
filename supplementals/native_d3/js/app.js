f9c379c7c9210f968c78fab9624fb9a553fc3eac
jcbain <jamescbain@gmail.com>
2019-10-25 09:40:12 -0600

    start scaffolding actual app ;

---
+++ supplementals/native_d3/js/app.js
@@ -0,0 +1,11 @@
+// chart parameters
+var chartWidth = 1000;
+var chartHeight = 800;
+var padding = 50;
+
+// svg
+var svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', chartWidth)
+    .attr('height', chartHeight);
+
