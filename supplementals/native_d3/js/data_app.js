1bba1e48d9eecf2e01ea19db7c9cf0d249bc788b
jcbain <jamescbain@gmail.com>
2019-11-07 15:14:04 -0700

    join data

--- supplementals/native_d3/js/data_app.js
+++ supplementals/native_d3/js/data_app.js
@@ -10,11 +10,21 @@ d3.json('data/genome_template.json').then(function(data){
 });
 
 
+
+
+
 Promise.all([
     d3.json('data/genome_individual.json'),
     d3.json('data/genome_template.json'),
 ]).then(function(files){
     let genomeg = files[0];
     let genomet = files[1];
-    console.log(genomeg);
+
+    genomet.forEach(function(position){
+        var result = genomeg.filter(function(g){
+            return g.position === position.position;
+        });
+        position.select_coef = (result[0] !== undefined) ? result[0].select_coef : null;
+    })
+    console.log(genomet);
 })
