2d7de46921f0d5fc9ead1302a6842609a485382b
jcbain <jamescbain@gmail.com>
2019-11-12 21:28:59 -0700

    add data prependers in javascript ;

--- supplementals/native_d3/js/data_app.js
+++ supplementals/native_d3/js/data_app.js
@@ -27,4 +27,50 @@ Promise.all([
         position.select_coef = (result[0] !== undefined) ? result[0].select_coef : 0;
     })
     console.log(genomet);
+    
+    const uniqueGenomes = (value, index, self) => {
+        return self.indexOf(value) === index;
+    }
+    
+    Array.prototype.unique = function() {
+        return this.filter(function (value, index, self){
+            return self.indexOf(value) === index;
+        });
+    }
+    
+    const sampleArray = [];
+    genomeg.forEach(function(d){ sampleArray.push(d.genome); });
+    console.log(sampleArray.unique());
+    
+    // sampleArray.unique().forEach(function(d){
+    //     var newResult = genomeg.filter(function(g){
+    //         return g.genome === d;
+    //     });
+    //     genomet.forEach(function(position){
+    //         var newNewResult = newResult.filter(function(n){
+    //             return n.position === position.position;
+    //         });
+    //         position.select_coef = (newNewResult[0] !== undefined) ? newNewResult[0].select_coef : 0;
+    //     })
+    // })
+    // console.log(genomet);
+    
+    let newTemplate = []
+    sampleArray.unique().forEach(function(d){
+        newTemplate = [...genomet]
+        var newResult = genomeg.filter(function(g){
+            return g.genome === d;
+        });
+        newTemplate.forEach(function(position){
+            var newNewResult = newResult.filter(function(n){
+                return n.position === position.position;
+            });
+            position.select_coef = (newNewResult[0] !== undefined) ? newNewResult[0].select_coef : 0;
+        });
+        console.log(newTemplate);
+    })
+    
+
+
+    console.log(genomeg.filter(function(d){return d.genome === "genome1";}))
 })
