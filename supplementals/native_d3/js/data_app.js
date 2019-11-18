089c54a950c6e2ceb50833d5faa2d1eee6a72f31
jcbain <jamescbain@gmail.com>
2019-11-18 11:51:04 -0700

    start the birt of a good idea and a bad git comment

--- supplementals/native_d3/js/data_app.js
+++ supplementals/native_d3/js/data_app.js
@@ -1,46 +1,62 @@
-let genome;
-let template;
-
-d3.json('data/genome_individual.json').then(function(data){
-    genome = data;
-});
-
-d3.json('data/genome_template.json').then(function(data){
-    template = data;
-});
-
-
-
-
+Array.prototype.unique = function(){
+    return this.filter(function(value, index, self){
+        return self.indexOf(value) === index;
+    });
+}
 
 Promise.all([
     d3.json('data/genome_individual.json'),
-    d3.json('data/genome_template.json'),
+    d3.json('data/genome_template.json')
 ]).then(function(files){
-    let genomeg = files[0];
-    let genomet = files[1];
+    let indivGenome = files[0],
+        templateGenome = files[1];
+    
 
-    genomet.forEach(function(position){
-        var result = genomeg.filter(function(g){
-            return g.position === position.position;
-        });
-        position.select_coef = (result[0] !== undefined) ? result[0].select_coef : 0;
-    })
-    console.log(genomet);
+    const genomeLabels = [];
+    indivGenome.forEach(g => genomeLabels.push(g.genome));
+    console.log(genomeLabels.unique());
     
-    const uniqueGenomes = (value, index, self) => {
-        return self.indexOf(value) === index;
+    indivFullGenome = {};
+    for(let genome of genomeLabels.unique()){
+        indivFullGenome[genome] = ;
     }
+    console.log(indivFullGenome);
+
+    console.log(indivGenome);
+    console.log(templateGenome);
+});
+
+
+
+
+// Promise.all([
+//     d3.json('data/genome_individual.json'),
+//     d3.json('data/genome_template.json'),
+// ]).then(function(files){
+//     let genomeg = files[0];
+//     let genomet = files[1];
+
+//     genomet.forEach(function(position){
+//         var result = genomeg.filter(function(g){
+//             return g.position === position.position;
+//         });
+//         position.select_coef = (result[0] !== undefined) ? result[0].select_coef : 0;
+//     })
+//     console.log(genomet);
     
-    Array.prototype.unique = function() {
-        return this.filter(function (value, index, self){
-            return self.indexOf(value) === index;
-        });
-    }
+//     const uniqueGenomes = (value, index, self) => {
+//         return self.indexOf(value) === index;
+//     }
     
-    const sampleArray = [];
-    genomeg.forEach(function(d){ sampleArray.push(d.genome); });
-    console.log(sampleArray.unique());
+//     Array.prototype.unique = function() {
+//         return this.filter(function (value, index, self){
+//             return self.indexOf(value) === index;
+//         });
+//     }
+    
+//     const sampleArray = [];
+//     genomeg.forEach(function(d){ sampleArray.push(d.genome); });
+//     console.log(sampleArray.unique());
     
     // sampleArray.unique().forEach(function(d){
     //     var newResult = genomeg.filter(function(g){
@@ -55,23 +71,25 @@ Promise.all([
     // })
     // console.log(genomet);
     
-    let newTemplate = []
-    sampleArray.unique().forEach(function(d){
-        newTemplate = [...genomet]
-        var newResult = genomeg.filter(function(g){
-            return g.genome === d;
-        });
-        newTemplate.forEach(function(position){
-            var newNewResult = newResult.filter(function(n){
-                return n.position === position.position;
-            });
-            position.select_coef = (newNewResult[0] !== undefined) ? newNewResult[0].select_coef : 0;
-        });
-        console.log(newTemplate);
-    })
+    // let newTemplate = []
+    // sampleArray.unique().forEach(function(d){
+    //     newTemplate = [...genomet]
+    //     var newResult = genomeg.filter(function(g){
+    //         return g.genome === d;
+    //     });
+    //     newTemplate.forEach(function(position){
+    //         var newNewResult = newResult.filter(function(n){
+    //             return n.position === position.position;
+    //         });
+    //         position.select_coef = (newNewResult[0] !== undefined) ? newNewResult[0].select_coef : 0;
+    //     });
+    //     console.log(newTemplate);
+    // })
+
+
     
 
 
-    console.log(genomeg.filter(function(d){return d.genome === "genome1";}))
-})
+//     console.log(genomeg.filter(function(d){return d.genome === "genome1";}))
+// })
 
